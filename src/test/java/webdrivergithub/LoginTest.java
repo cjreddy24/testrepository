494ea0a1007376077ba20440879f492cba5d1842
jagan <cjagadishwarreddy@gmail.com>
2021-09-14 23:59:20 +0530

    1st version

---
+++ src/test/java/webdrivergithub/LoginTest.java
@@ -0,0 +1,24 @@
+package webdrivergithub;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginTest {
+
+	
+
+	@Test
+	public void launch() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://gmail.com");
+	}
+	
+}
+
+
